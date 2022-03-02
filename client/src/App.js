a0e34bfd82aa03ef982e6b0724716bc85077c702
Mario Franca <mfranca@pop-os.localdomain>
2022-03-02 10:49:51 +0000

    Basic UI built. The UI only creates the tasks. Backend has all endpoints working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import CreateTask from './components/CreateTask';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>GNAR BACKEND TEST</h1>
-        <p>Testing display of the components</p>
+        <div className='App-header'>
+          <h1>GNAR BACKEND TEST</h1>
+          <p>This is a simple UI for creating tasks</p>
+          <br />
+          <ArrowDownwardIcon />
+        </div>
         <CreateTask />
       </header>
     </div>
